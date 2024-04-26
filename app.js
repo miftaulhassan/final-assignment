9e27cefe8625981f4252863b0df09ee0a07ca7f2
miftaulhassan <158443872+miftaulhassan@users.noreply.github.com>
2024-04-26 11:27:31 +0600

    Update app.js

--- app.js
+++ app.js
@@ -2,3 +2,14 @@ const searchInput = document.getElementById('searchInput');
 const searchButton = document.getElementById('searchButton');
 const mealResults = document.getElementById('mealResults');
 const showAllButton = document.getElementById('showAllButton');
+
+
+let meals = [];
+let visibleMeals = [];
+
+
+async function fetchMeals(searchText) {
+  const response = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`);
+  const data = await response.json();
+  return data.meals;
+}
